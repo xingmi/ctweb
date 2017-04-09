777e20bcbe38e2d87b2f9560d16663666b90dc1e
luliangshu <523994707>
2017-04-09 11:36:42 +0800

    啊

--- res/controllers/index.js
+++ res/controllers/index.js
@@ -2,10 +2,10 @@ var Vue = require('vue');
 var Header = require('../components/Header.vue');
 var Footer = require('../components/Footer.vue')
 var HomeHeader = require('../components/homeHeader.vue');
+var ApplyModule = require('../components/applyModule.vue')
 var Config = require('../config/globalMain');
 var utility = require('../config/utility');
 var Toast = require('../widget/toast');
-var bus = require('../config/bus')
 var productList = new Vue({
     el : '.index',
     data : {
@@ -14,7 +14,8 @@ var productList = new Vue({
     components: {
       'top-header': Header,
       'bottom-footer' : Footer,
-      'home-header'   : HomeHeader
+      'home-header'   : HomeHeader,
+      'apply-module' : ApplyModule
     },
     created : function(){
       
