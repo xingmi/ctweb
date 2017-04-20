d64a338e7946d229cc586a75b55428cf11bb60ed
luliangshu <523994707>
2017-04-20 22:03:47 +0800

    dd

--- res/controllers/collaboration.js
+++ res/controllers/collaboration.js
@@ -4,8 +4,6 @@ var Footer = require('../components/Footer.vue')
 var ApplyModule = require('../components/applyModule.vue')
 var HomeHeader = require('../components/homeHeader.vue')
 var Config = require('../config/globalMain');
-var utility = require('../config/utility');
-var Toast = require('../widget/toast');
 
 new Vue({
     el : '.collaboration',
--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
     historyApiFallback: true,
     noInfo: true
   },
-  devtool: '#eval-source-map'
+  devtool: 'source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
